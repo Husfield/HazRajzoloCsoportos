40fe805d6b79e3f4f09ee2f9368606541e186fb8
0M1lan0 <ignacz.milan@diak.szamalk-szalezi.hu>
2026-05-04 08:27:46 +0200

    HazAlja

---
+++ RajzolasCsoportos.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.speed(0)
+turtle.width(3)
+
+szog = 90
+hossz = 100
+i = 0
+
+while i < 4:
+    turtle.forward(hossz)
+    turtle.left(szog)
+    i += 1
+
+turtle.left(szog)
+turtle.forward(hossz)
+
+turtle.done()
